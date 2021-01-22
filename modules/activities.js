d0a20047de9fb659635d8408a8c60b5a7f072f8a
enyichiaagu <enyichiaagu@gmail.com>
2021-01-23 00:30:32 +0100

    mods

--- modules/activities.js
+++ modules/activities.js
@@ -1,35 +1,35 @@
 const activities = `
 <div class="activities">
     <div class="activity">
-        <img alt="DJing's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.2885-15/e35/c45.512.1035.1035a/s150x150/106273079_2642031592676530_15452110712306974_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=igd8PjNXyQoAX_e2z0-&amp;tp=1&amp;oh=b91509947dc10877aa56772dd85d9825&amp;oe=600B12BA">
+        <img alt="DJing's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.2885-15/e35/c45.512.1035.1035a/s150x150/106273079_2642031592676530_15452110712306974_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=igd8PjNXyQoAX8hwDK8&tp=1&oh=b3defff957401859fd68e04f85d59f0c&oe=600DB5BA">
         <p>DJing</p>
     </div>
     <div class="activity">
-        <img alt="Collabs's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.12442-15/e15/c102.360.497.497a/s150x150/107248569_2620987841550478_457525924260294724_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=LBy2CEOLKMIAX9mBRZY&amp;tp=1&amp;oh=a2bdb98b944cc11aa9b4fd87b3d2ae69&amp;oe=600B0F67">
+        <img alt="Collabs's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.12442-15/e15/c102.360.497.497a/s150x150/107248569_2620987841550478_457525924260294724_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=8EpZKLfhZjIAX8w30Ji&tp=1&oh=959a67080c4251ce87ea8d02309d7bac&oe=600DB267">
         <p>Collabs</p>
     </div>
     <div class="activity">
-        <img alt="Music's profile picture" class="NCYx-" src="https://instagram.fbni1-2.fna.fbcdn.net/v/t51.2885-15/e35/c54.518.1035.1035a/s150x150/106246042_566747764213249_2372639458583392537_n.jpg?_nc_ht=instagram.fbni1-2.fna.fbcdn.net&amp;_nc_cat=101&amp;_nc_ohc=7WwE6bUWTe8AX_tbqvu&amp;tp=1&amp;oh=0164768a5ca2898e77ca38e914e9e2e0&amp;oe=600ADD11">
+        <img alt="Music's profile picture" class="NCYx-" src="https://instagram.fbni1-2.fna.fbcdn.net/v/t51.2885-15/e35/c54.518.1035.1035a/s150x150/106246042_566747764213249_2372639458583392537_n.jpg?_nc_ht=instagram.fbni1-2.fna.fbcdn.net&_nc_cat=101&_nc_ohc=po0OHrNj4iIAX-SEvK7&tp=1&oh=65fc331e1df269d4528a3eb312f2d387&oe=600D8011">
         <p>Music</p>
     </div>
     <div class="activity">
-        <img alt="Press's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.12442-15/e15/c24.134.662.662a/s150x150/107148185_278098703257221_3617311764793798981_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&amp;_nc_cat=109&amp;_nc_ohc=64kQoX2UEPAAX9THbxy&amp;tp=1&amp;oh=64d54e538e8090417fe9853d49906ac5&amp;oe=600AFBCB">
+        <img alt="Press's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.12442-15/e15/c24.134.662.662a/s150x150/107148185_278098703257221_3617311764793798981_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=64kQoX2UEPAAX9CN28W&tp=1&oh=3b692e09a627aed5775aa6e431c46544&oe=600D9ECB">
         <p>Press</p>
     </div>
     <div class="activity">
-        <img alt="Radio's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.2885-15/e35/c66.553.1035.1035a/s150x150/106236347_1467569436964270_7885937919983396725_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&amp;_nc_cat=110&amp;_nc_ohc=69CuNmqBGuAAX8kuun4&amp;tp=1&amp;oh=fa6a5810417fc1d65ba8665193d14d1c&amp;oe=600AD799">
+        <img alt="Radio's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.2885-15/e35/c66.553.1035.1035a/s150x150/106236347_1467569436964270_7885937919983396725_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=hRR5kGV569sAX_DzIlv&tp=1&oh=1dbc56a8cb18af0390059222a82fcde1&oe=600D7A99">
         <p>Radio</p>
     </div>
     <div class="activity">
-        <img alt="Cactus Party's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.437.1125.1125a/s150x150/81451382_4150754211631982_5245142746187458076_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&amp;_nc_cat=102&amp;_nc_ohc=rI4OM28c2vAAX9Hv40y&amp;tp=1&amp;oh=b9e0e56bc25aa0a4675d4e785d0ca7c5&amp;oe=600AF9AB">
+        <img alt="Cactus Party's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.437.1125.1125a/s150x150/81451382_4150754211631982_5245142746187458076_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=CKDaxtTPOiIAX-JYnRd&tp=1&oh=827327a15cac3923e30647dd6537665b&oe=600D9CAB">
         <p>Cactus Party</p>
     </div>
     <div class="activity">
-        <img alt="Charity's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.437.1125.1125a/s150x150/106098686_361103031536987_7305599966247481426_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&amp;_nc_cat=111&amp;_nc_ohc=tdKBz4QoqiwAX9yScvI&amp;tp=1&amp;oh=b710a9bcb095d8f7a6c858644be99aa7&amp;oe=600AACC3">
+        <img alt="Charity's profile picture" class="NCYx-" src="https://instagram.fbni1-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.437.1125.1125a/s150x150/106098686_361103031536987_7305599966247481426_n.jpg?_nc_ht=instagram.fbni1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=tdKBz4QoqiwAX-qHHQ1&tp=1&oh=6ead75d164877d04abe9702e03ee1484&oe=600DF883">
         <p>Charity</p>
     </div>
     <div class="activity">
-        <img alt="Vlog's profile picture" class="NCYx-" src="https://instagram.fbni1-2.fna.fbcdn.net/v/t51.12442-15/e15/c24.246.662.662a/s150x150/107566469_327678694926325_2903298070991032601_n.jpg?_nc_ht=instagram.fbni1-2.fna.fbcdn.net&amp;_nc_cat=101&amp;_nc_ohc=FAUG0QZC0OUAX90d6VB&amp;tp=1&amp;oh=f4f1567798fd97d1cb78b4df04e7cb68&amp;oe=600AB3DD">
+        <img alt="Vlog's profile picture" class="NCYx-" src="https://instagram.fbni1-2.fna.fbcdn.net/v/t51.12442-15/e15/c24.246.662.662a/s150x150/107566469_327678694926325_2903298070991032601_n.jpg?_nc_ht=instagram.fbni1-2.fna.fbcdn.net&_nc_cat=101&_nc_ohc=FAUG0QZC0OUAX8zCaXe&tp=1&oh=1095af303006ea48101be20cfab3a2c2&oe=600DFF9D">
         <p>Vlog</p>
     </div>
 </div>
