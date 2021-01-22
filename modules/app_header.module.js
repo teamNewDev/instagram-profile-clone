2fd306171fc9ddc8c9066aea455c3281bb4f40b5
John Chimaobi <45017704+ijelechimaobi@users.noreply.github.com>
2021-01-22 18:29:37 +0100

    commit

---
+++ modules/app_header.module.js
@@ -0,0 +1,26 @@
+const Header = () => `
+        <nav class="app_nav">
+            <div class="app_nav--left">
+                <a href="#" class="app_link--icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left">
+                        <line x1="19" y1="12" x2="5" y2="12"></line>
+                        <polyline points="12 19 5 12 12 5"></polyline>
+                    </svg>
+                </a>
+                <a href="#" class="app_link--title">cuppymusic
+                    <img src="https://th.bing.com/th/id/Rca1915315f01527947a650bc92b26f1a?rik=uPMfwAQ3B4z7%2fQ&riu=http%3a%2f%2fcdn181.picsart.com%2f229913690016202.png&ehk=dDCt84GGGVIO7c9TQg3t9SRS4hPmD3GoiWs64MY5rWM%3d&risl=&pid=ImgRaw" class="title_verified" />
+                </a>
+            </div>
+            <div class="app_nav--right">
+                <a href="#" class="app_link--icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical">
+                        <circle cx="12" cy="12" r="1"></circle>
+                        <circle cx="12" cy="5" r="1"></circle>
+                        <circle cx="12" cy="19" r="1"></circle>
+                    </svg>
+                </a>
+            </div>
+        </nav>`;
+export default Header;
---
+++ modules/bio.module.js
@@ -0,0 +1,63 @@
+const Bio = () => `
+        <div class="app_intro--container">
+
+            <div class="app_intro--top">
+
+
+                <div class="app_intro--top_dp--container">
+                    <img src="https://instagram.fabv2-1.fna.fbcdn.net/v/t51.2885-19/s320x320/130447775_695242761181127_6570908212231106919_n.jpg?_nc_ht=instagram.fabv2-1.fna.fbcdn.net&_nc_ohc=gTqn5sI4CM0AX_9YTLe&tp=1&oh=188460651763b3a776e0571d3cc241d3&oe=603375C6" alt="cuppymusic | instragram" class="top_dp--img">
+                </div>
+
+                <div class="app_intro--counter_set--container">
+
+                    <!-- Item -->
+                    <div class="counter_set--item">
+                        <div class="item_bold">4,259</div>
+                        <div class="item_light">Posts</div>
+                    </div>
+
+                    <!-- Item -->
+                    <div class="counter_set--item">
+                        <div class="item_bold">6.8M</div>
+                        <div class="item_light">Followers</div>
+                    </div>
+
+                    <!-- Item -->
+                    <div class="counter_set--item">
+                        <div class="item_bold">291</div>
+                        <div class="item_light">Following</div>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div class="app_intro--about">
+
+                <p class="about_text--bold">cuppy</p>
+                <p class="about_text">☆ DJ, Artist, & Philanthropist 🇳🇬💕<br />
+                ☆ Host of <a href="#" class="about_text--link">#AfricaNow</a> on Apple Music <br />
+                ☆ Founder of... <a href="#" class="about_text--link">more</a> <br />
+                Watch my NEW #LittyLit Video ⬇️</p>
+
+            </div>
+
+            <div class="app_intro--cta">
+                <a href="#" class="app_intro--cta_btn _blue">Follow</a>
+                <a href="#" class="app_intro--cta_btn">Message</a>
+                <a href="#" class="app_intro--cta_btn">Email</a>
+                <a href="#" class="app_intro--cta_btn _small">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
+                    <polyline points="6 9 12 15 18 9"></polyline>
+                </svg></a>
+            </div>
+
+
+            <div class="app_intro--tag_list"></div>
+            
+
+        </div>`;
+
+export default Bio;
