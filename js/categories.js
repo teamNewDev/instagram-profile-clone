cc1df430e3b9d162b77ba11262e1be7c1cc99325
Francis Ali <githubemail61@gmail.com>
2021-01-24 17:48:53 +0100

    My pull request

---
+++ js/categories.js
@@ -0,0 +1,52 @@
+const _catg = `
+<div class="circle-link">
+  <div>
+    <img src="images/djing.jpg" width="100%" height="100%"/>
+  </div>
+  <span class="blue-grey-text">DJing</span>
+</div>
+<div class="circle-link">
+  <div>
+    <img src="images/collabs.jpg" width="100%" height="100%"/>
+  </div>
+  <span class="blue-grey-text">Collabs</span>
+</div>
+<div class="circle-link">
+  <div>
+    <img src="images/music.jpg" width="100%" height="100%"/>
+  </div>
+  <span class="blue-grey-text">Music</span>
+</div>
+<div class="circle-link">
+  <div>
+    <img src="images/press.jpg" width="100%" height="100%"/>
+  </div>
+  <span class="blue-grey-text">Press</span>
+</div>
+<div class="circle-link">
+  <div>
+    <img src="images/radio.jpg" width="100%" height="100%"/>
+  </div>
+  <span class="blue-grey-text">Radio</span>
+</div>
+<div class="circle-link">
+  <div>
+    <img src="images/cactus.jpg" width="100%" height="100%"/>
+  </div>
+  <span class="blue-grey-text">Cactus...</span>
+</div>
+<div class="circle-link">
+  <div>
+    <img src="images/charity.jpg" width="100%" height="100%"/>
+  </div>
+  <span class="blue-grey-text">Charity</span>
+</div>
+<div class="circle-link">
+  <div>
+    <img src="images/vlog.jpg" width="100%" height="100%"/>
+  </div>
+  <span class="blue-grey-text">Vlog</span>
+</div>
+`;
+
+export default _catg;
---
+++ js/popup.js
@@ -0,0 +1,12 @@
+const _popupContent = `
+<div class="popup">
+  <div class="option">Block this user</div>
+  <div class="divider"></div>
+  <div class="option">Restrict</div>
+  <div class="divider"></div>
+  <div class="option">Report User</div>
+  <div class="divider"></div>
+  <div class="option cancel">Cancel</div>
+</div>
+`;
+export default _popupContent;
