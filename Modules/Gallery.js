14338a07855fa2f66596609e68603dce2eafa948
Maurice <mauricejahwizwid@gmail.com>
2021-01-24 16:20:10 +0100

    Add files via upload
    couldn't see any output...what could be wrong?

---
+++ Modules/Gallery.js
@@ -0,0 +1,27 @@
+
+
+const Gallery =() => `
+
+<section class = "gallery">
+             <div class = "gallery-title"></div>
+
+<article class ="gallery-photos">
+                  <div class="photo"><a href=""><img src="https://sirkenayo.com/wp-content/uploads/2019/11/cuppy-1-700x513-700x513.jpg" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""> <img src="https://file.naijauto.com/2019/02/01/u0GUC7wC/dj-cuppy-rolls-royce1-6fc1.png" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""><img src="https://i0.wp.com/www.ghgossip.com/wp-content/uploads/2020/01/DJ-Cuupy.png?fit=740%2C484&ssl=1" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""><img src="https://www.bellanaijastyle.com/wp-content/uploads/2018/10/cuppy.jpg" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""><img src="https://i.pinimg.com/originals/1e/ac/ad/1eacad2602d206b000a05a244d36cf13.jpg" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""><img src="https://i0.wp.com/www.vanguardngr.com/wp-content/uploads/2021/01/images-15.jpeg?fit=739%2C415&ssl=1" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""><img src="https://www.36ng.ng/wp-content/uploads/2019/03/cuppymusic_brirwzvd65k6257920632935670981.jpg" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""><img src="https://1.bp.blogspot.com/-2a6vKs0wYQo/Xnw-uM8KKvI/AAAAAAAISH8/8ldMaWMUVAAwDp_wcGG65jVcbbK5l6qNACLcBGAsYHQ/s1600/1585185420483_adobe.jpg" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""><img src="https://ranthq.org/blog/wp-content/uploads/2020/04/3F0614CA-A04A-46AE-AE1A-F1C61E905181-748x720.jpeg" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""><img src="https://i1.wp.com/www.informationng.com/wp-content/uploads/2019/09/DJ-Cuppy-lee.png?ssl=1" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""><img src="https://akpraise.ng/wp-content/uploads/2019/10/img_20191018_1030205251727907979239864.jpg" width= "300" height="300" alt="photo"> </a></div>
+                  <div class="photo"><a href=""><img src="https://mammypi.com/wp-content/uploads/2020/09/cuppymusic_20200930_2.jpg" width= "300" height="300" alt="photo"> </a></div>
+            </article>
+
+        </section>
+
+        `
+
+        export default Gallery;
---
+++ Modules/Header.js
@@ -0,0 +1,23 @@
+
+
+const Header = () => `
+
+<header>
+<img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram-logo" class = "instagram-logo">
+<input type="search" name="" id=""  placeholder="Search"> <i class="fas fa-search"></i>
+<nav>
+    <ul>
+    <li><a href="#"><i class="fas fa-home"></i></a></li>
+    <li><a href="#"><i class="far fa-paper-plane"></i> </a></li>
+    <li><a href="#"><i class="far fa-compass"></i></a></li>
+    <li><a href="#"> <i class="far fa-heart"></i></a></li>
+    <li><a href="#"><img src="./images/envelope.png" alt="" width= "20px" height="20px"></a></li>
+    </ul>
+</nav>
+</header>
+
+
+`
+
+
+export default Header;
