b915c515a194d17887f8fc8c6ef6e399a2494bbe
Ese Curtis <47295790+EseCurtis@users.noreply.github.com>
2021-01-26 07:37:47 +0100

    Delete assets/scripts directory

--- assets/scripts/modules/HTMLCommenter.js
+++
@@ -1,7 +0,0 @@
-//a simple html commenting function to avoid rewriting html comment symbols
-const HTMLComment = (commentContent = "")=>{
-    return `<!-- ${commentContent} -->`;
-}
-
-//export HTMLComment as a module
-export default HTMLComment;
