48c5e88c3742f9622dcc2feb7e1af00e00947e80
Ese Curtis <47295790+EseCurtis@users.noreply.github.com>
2021-01-26 07:38:51 +0100

    Add files via upload

---
+++ assets/scripts/modules/AppData/HighlightsData.js
@@ -0,0 +1,42 @@
+/* 
+** hightlight data module holds array for all hightlight items 
+** the array holds objects with the following fields
+** title: for setting the item title
+** poster: for the poster image rather backgrund image of the item
+*/
+const HighlightsData = [
+    {
+        poster:"https://instagram.fabb1-1.fna.fbcdn.net/v/t51.2885-15/e35/c45.512.1035.1035a/s150x150/106273079_2642031592676530_15452110712306974_n.jpg?_nc_ht=instagram.fabb1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=Z1dtS7Ccx9UAX_dksE3&tp=1&oh=ff29d3c5a00bb071caffd42f47888662&oe=600F073A",
+        title:"Djing"
+    },
+    {
+        poster:"https://instagram.fabb1-1.fna.fbcdn.net/v/t51.12442-15/e15/c102.360.497.497a/s150x150/107248569_2620987841550478_457525924260294724_n.jpg?_nc_ht=instagram.fabb1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=8EpZKLfhZjIAX8qCsQs&tp=1&oh=4c32e9afd108230f8133dc65950bcc71&oe=600F03E7",
+        title:"Collabs"
+    },
+    {
+        poster:"https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/c54.518.1035.1035a/s150x150/106246042_566747764213249_2372639458583392537_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&amp;_nc_cat=101&amp;_nc_ohc=po0OHrNj4iIAX_VBJXz&amp;tp=1&amp;oh=fbfdd6a8ba76c9438e4220873f8ff39a&amp;oe=600CD751",
+        title:"Music"
+    },
+    {
+        poster:"https://instagram.flos1-1.fna.fbcdn.net/v/t51.12442-15/e15/c24.134.662.662a/s150x150/107148185_278098703257221_3617311764793798981_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=64kQoX2UEPAAX-vk_wB&tp=1&oh=d3ec32961f5756fb564cdf5aeb9d2345&oe=600CF60B",
+        title:"Press"
+    },
+    {
+        poster:"https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/c66.553.1035.1035a/s150x150/106236347_1467569436964270_7885937919983396725_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=hRR5kGV569sAX_UvaaX&tp=1&oh=3f7b58071fb6a9ce6c48e1103dd9e7f2&oe=600CD1D9",
+        title:"Radio"
+    },
+    {
+        poster:"https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.437.1125.1125a/s150x150/81451382_4150754211631982_5245142746187458076_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=CKDaxtTPOiIAX-11SU4&tp=1&oh=1f39dc54d0086258f588a98903fb6f45&oe=600CF3EB",
+        title:"Cactus Party"
+    },
+    {
+        poster:"https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.437.1125.1125a/s150x150/106098686_361103031536987_7305599966247481426_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=tdKBz4QoqiwAX_XUBST&tp=1&oh=c4d8c1d0e3cbb6e693873432c7715bf9&oe=600CA703",
+        title:"Charity"
+    },
+    {
+        poster:"https://instagram.flos1-1.fna.fbcdn.net/v/t51.12442-15/e15/c24.246.662.662a/s150x150/107566469_327678694926325_2903298070991032601_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=FAUG0QZC0OUAX_Afupq&tp=1&oh=beb618dc7fce965ded5091ae8b1c4242&oe=600CAE1D",
+        title:"Vlog"
+    }
+];
+
+export default HighlightsData;
